2937ab356cfd2f16a17a836db0b7822d5abbd8a0
Oluwafisayomi Ogunyemi <oluwafisayomiogunyemi@gmail.com>
2020-12-11 19:52:57 +0100

    Update Practice_1.java

--- Practice_1.java
+++ Practice_1.java
@@ -31,5 +31,40 @@ public class Main {
             }
             System.out.println(sum1);
             
+	    package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int s1 = 24;
+            System.out.println(s1);
+
+            String name= "Fisayo";
+            System.out.println(name);
+
+            int Fisayo_score = 30;
+            int Segun_score = 24;
+
+            System.out.println(Fisayo_score - Segun_score);
+
+            int sum = 0;
+            int counter = 10;
+
+            while (counter <=20) {
+                sum = sum + counter;
+                counter = counter + 1;
+            }
+            System.out.println(sum);
+
+            int sum1 = 1;
+            int counter1 = 5;
+            while (counter1 <= 20) {
+                sum1 = sum1 + counter1;
+                counter1 = counter1 - 1;
+            }
+            System.out.println(sum1);
+            
+    }
+}
     }
 }
